093b5d19e07783ebd9229746c394290c081106f5
Pedro Freitas <pedrofreitas1410@gmail.com>
2018-04-06 13:21:48 +0100

    Main

--- src/main.c
+++ src/main.c
@@ -25,22 +25,24 @@ int main(int argc, char **argv){
 		return(0);
   }
 
-  char *doc;
-  doc = argv[1];
-  xmlDocPtr doc2 = xmlParseFile(doc);
-    //GTree *arv_users = g_tree_new((GCompareFunc) idusercompare);
+  char *pos;
+  pos = argv[1];
+  xmlDocPtr pos2 = xmlParseFile(pos);
+  GTree *arv_users = g_tree_new((GCompareFunc) idusercompare);
   GTree *arv_posts = g_tree_new((GCompareFunc) idpostcompare);
 
+  char *us;
+  us = argv[2];
+  xmlDocPtr us2 = xmlParseFile(us);
 
-
-  //userInfo(doc2, arv_users);
-  postsInfo(doc2, arv_posts);
+  userInfo(us2, arv_users);
+  postsInfo(pos2, arv_posts);
 
   Key pid = createKey(4);
   Post p = (Post)g_tree_lookup(arv_posts, pid);
   assert(p!=NULL);
-  g_tree_foreach(arv_posts, (GTraverseFunc)iterator, NULL);
-  g_tree_free(arv_posts);
+//  g_tree_foreach(arv_posts, (GTraverseFunc)iterator, NULL);
+  //g_tree_free(arv_posts);
   //Key uid = createKey(1);
   //User u = (User)g_tree_lookup(arv_users, uid);
   //assert(u!=NULL);

d0433b3e1bba90a0b1f57977274ebb8e29cda958
CarlaCruz146 <carla.cruz.247@gmail.com>
2018-04-24 00:28:03 +0100

    Documentação

--- src/lib/mypost.c
+++ src/lib/mypost.c
@@ -20,11 +20,19 @@ struct arrayd{
   long per;
 };
 
+/**
+  *@brief   Função que dará cast ao void recebido para uma data e depois dará free à mesma.
+  *@param   Recebe um apontador.
+*/
 void destroyDate (void *d){
   Date aux = (Date) d;
   free_date(aux);
 }
 
+/**
+  *@brief   Função que cria um array dinâmico que conterá o array, o seu tamanho o que já fora usado e também teŕa contadores para o número de perguntas e respostas.
+  *@param   Recebe o comprimente que o array terá.
+*/
 ArrayD createArray(long comp){
   ArrayD a = malloc (sizeof(struct arrayd));
   a->array = malloc(comp * sizeof(struct post));
@@ -34,33 +42,56 @@ ArrayD createArray(long comp){
   a->per = 0;
   return a;
 }
-
+/**
+  *@brief   Função que retorna o número de perguntas contidas naquele array.
+  *@param   Recebe um array dinâmico.
+*/
 long getPer(ArrayD d){
   if(d) return d->per;
   else return -1;
 }
 
+/**
+  *@brief   Função que retorna o número de respostas contidas naquele array.
+  *@param   Recebe um array dinâmico.
+*/
 long getRes(ArrayD d){
   if(d) return d->res;
   else return -1;
 }
 
+/**
+  *@brief   Função que retorna o tamanho allocado para aquele array.
+  *@param   Recebe um array dinâmico.
+*/
 long getSize(ArrayD d){
   if(d) return d->size;
   else return -1;
 }
 
+/**
+  *@brief   Função que retorna o tamanho usado naquele array.
+  *@param   Recebe um array dinâmico.
+*/
 long getUsed(ArrayD d){
   if(d) return d->used;
   else return -1;
 }
 
+/**
+  *@brief   Função que retorna o Post contido no array, naquela posição i.
+  *@param   Recebe um array dinâmico e um índice.
+*/
 Post getInd(ArrayD d, int i){
   if (d)
     return (d->array[i]);
   else return NULL;
 }
 
+/**
+  *@brief   Função que insere um Post no array.
+  *@param   Recebe um array dinâmico e um determinado Post.
+*/
 void insereArray(ArrayD a, Post p){
   if (a-> used == a->size) {
     a->size *= 2;
@@ -72,6 +103,10 @@ void insereArray(ArrayD a, Post p){
   a->array[a->used++] = p;
 }
 
+/**
+  *@brief   Função que dará cast ao void, tornando-o num array dinâmico e depois dará free a essa estrutura.
+  *@param   Recebe um apontador void.
+*/
 void freeArray(void *a){
   ArrayD aux = (ArrayD) a;
   if(aux){
@@ -80,11 +115,18 @@ void freeArray(void *a){
   }
 }
 
+/**
+*@brief   Função que dará init a um post, retornando o mesmo.
+*/
 Post initPost(){
   Post p = malloc(sizeof(struct post));
   return p;
 }
 
+/**
+  *@brief   Função que com base nos parâmetros irá fazer a criação de um Post.
+  *@param   Recebe um id, o type, o parent id, o score, o viewcount, a data de criação, o criador, o número de comentário e o titulo.
+*/
 Post createPost(long id, int type, long pid, int score, int vcount, Date date, long owner, int numcom, char* titulo){
   Post p = malloc(sizeof(struct post));
   p->id = id;
@@ -99,31 +141,55 @@ Post createPost(long id, int type, long pid, int score, int vcount, Date date, l
   return p;
 }
 
+/**
+  *@brief   Função que retornará o id de um dado Post.
+  *@param   Recebe um Post.
+*/
 long getPostId(Post p){
   if(p) return p->id;
   return -1;
 }
 
+/**
+  *@brief   Função que retornará o tipo de um dado Post.
+  *@param   Recebe um Post.
+*/
 int getPostType(Post p){
   if(p) return p->type;
   return -1;
 }
 
+/**
+  *@brief   Função que retornará o parent id do Post recebido.
+  *@param   Recebe um Post.
+*/
 long getPid(Post p){
   if(p) return p->pid;
   return -1;
 }
 
+/**
+  *@brief   Função que retornará o score de um dado Post.
+  *@param   Recebe um Post.
+*/
 int getPostScore(Post p){
   if(p) return p->score;
   return -1;
 }
 
+/**
+  *@brief   Função que retornará o view count do Post.
+  *@param   Recebe um Post.
+*/
 int getPostVCount(Post p){
   if(p) return p->vcount;
   return -1;
 }
 
+/**
+  *@brief   Função que retornará a data de criação do Post recebido.
+  *@param   Recebe um Post.
+*/
 Date getPostDate(Post p){
   if (p){
   int day = get_day(p->date);
@@ -135,21 +201,37 @@ Date getPostDate(Post p){
   return NULL;
 }
 
+/**
+  *@brief   Função que retornará o criador do Post.
+  *@param   Recebe um Post.
+*/
 long getPostOwner(Post p){
   if(p) return p->owner;
   return -1;
 }
 
+/**
+  *@brief   Função que retornará o número de comentários de um dado Post.
+  *@param   Recebe um Post.
+*/
 int getPostNumCom(Post p) {
   if (p) return p->numcom;
   return -1;
 }
 
+/**
+  *@brief   Função que retornará o título de um dado Post.
+  *@param   Recebe um Post.
+*/
 char* getPostTitulo(Post p){
   if(p) return mystrdup(p->titulo);
   return NULL;
 }
 
+/**
+  *@brief   Função que dará free de um Post, depois de ser realizado o cast.
+  *@param   Recebe um apontador.
+*/
 void freePost(void *p){
   Post aux = (Post) p;
   if(aux) {

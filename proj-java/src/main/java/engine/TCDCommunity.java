b996f969054186c5f58f2549903c8130e083736d
CarlaCruz146 <carla.cruz.247@gmail.com>
2018-06-12 20:04:25 +0100

    tcd

--- proj-java/src/main/java/engine/TCDCommunity.java
+++ proj-java/src/main/java/engine/TCDCommunity.java
@@ -130,29 +130,142 @@ public class TCDCommunity implements TADCommunity {
     }
 */
 
-    // Query 7
-/*
+        // Query 7
+    /**
+     * Dado um intervalo de tempo arbitrário devolver os id's das N perguntas com mais perguntas.
+     * @param long Id do User
+     * @param LocalDate data inicial
+     * @param LocalDate data final
+     * @return List<Long> com short bio do user e o id dos ultimos 10 posts
+     */
     public List<Long> mostAnsweredQuestions(int N, LocalDate begin, LocalDate end) {
+        TreeMap<LocalDate,ArrayList<Post>> posts = this.com.getPosts().getPostDatas();
+        ArrayList<Post> datepost = null;
+        Set<Post> retS = new TreeSet<>(new ComparatorPostNRes());
+
+        while(!begin.equals(end.plusDays(1))){
+            datepost = posts.get(begin);
+            for(Post p : datepost){
+                    retS.add(p);
+            }
+            begin = begin.plusDays(1);
+        }
+        List<Long> list = new ArrayList<>();
+        List<Post> aux = new ArrayList<>();
+        int i = 0;
+        for(Post p : retS){
+            aux.add(i,p);
+            i++;
+        }
+        for(int j = 0; j < N; j++){
+            list.add(j,Long.valueOf(aux.get(j).getPostId()));
+        }
+        return list;
+
     }
-*/
+
     // Query 8
-    /*
+    /**
+     * Dado uma palavra devolver uma lista com is Id's das N perguntas cujo o titulo contenha
+     * @param int N
+     * @param String palavra 
+     * @return List<Long> com todas as perguntas cujo título contenha a palavra
+     */
     public List<Long> containsWord(int N, String word) {
+        PostBD posts = com.getPosts();
+        Set<Post> postsS = new TreeSet<>(new ComparatorPostDate());
+        List<Post> aux = new ArrayList<>();
+        List<Long> ret = new ArrayList<Long>();
+        System.out.println("oi");
+        for(Post p : posts.getPostMap().values()){
+            if(p.getPostType() == 1 && p.getPostTitulo().contains(word))
+                postsS.add(p);   
+        }
+        int i = 0;
+        for(Post p : postsS){
+            aux.add(i,p.clone());
+            i++;
+        }
+        for(i = 0; i < N; i++)
+           ret.add(i,aux.get(i).getPostId());
+
+        return ret;
     }
-    */
 
     // Query 9
-   /*
+    /**
+    *@brief   Retorna as últimas N perguntas (por ordem cronológica inversa) em que participaram dois utilizadores específicos.
+    *@param   long id do user 1.
+    *@param   long id do user 2.
+    *@param   int N.
+    *return   LONG_list ID's das perguntas.
+    */
     public List<Long> bothParticipated (int N, long id1, long id2){
+        Integer key1 = (int)(long)id1;
+        Integer key2 = (int)(long)id2;
+        ArrayList<Post> posts1 = new ArrayList<>((this.com.getUsers().getUserMap().get(id1).getUserPosts()));
+        ArrayList<Post> posts2 = new ArrayList<>((this.com.getUsers().getUserMap().get(id2).getUserPosts()));
+
+        Set<Integer> aux = new TreeSet<>();
+        TreeSet<Post> setaux = new TreeSet<Post>(new ComparatorPostDate());
+
+        for(Post p1 : posts1)
+            for(Post p2 : posts2){
+                if(p1.getPostType() == 2 && p2.getPostType() == 1 && p2.getPostId() == p1.getPostPid())
+                    setaux.add(p2.clone());
+                 
+                if(p1.getPostType() == 1 && p2.getPostType() == 2 && p2.getPostPid() == p1.getPostId())   
+                    setaux.add(p1.clone());
+
+                if(p1.getPostType() == 2 && p2.getPostType() == 2 && p2.getPostPid() == p1.getPostPid()){
+                    Post p3 = this.com.getPosts().getPostMap().get(p1.getPostPid());
+                    setaux.add(p3.clone());
+                }
+
+            }
+        List<Long> res = new ArrayList<>();
+        Post post = null;
+
+        Iterator<Post> it = setaux.iterator();
+
+        int i = 0;
+        while(it.hasNext() && i<N){
+            post = it.next();
+            post = setaux.first();
+            res.add(post.getPostId());
+            i++;
+        }
+
+        return res;
     }
     
-    */
-
     // Query 10
-    /*
-    public long betterAnswer(long id){     
+    /**
+     * Dado o id de um post obter a melhor resposta
+     * @param long id do post 
+     * @return long id da melhor resposta
+     */
+    public long betterAnswer(long id){
+        Map<Long,Post> posts = com.getPosts().getPostMap();
+        Set<Post> sposts = new HashSet<>();
+        long ret = -1;
+        double x, media = 0;
+        for(Post p : posts.values())
+            if(p.getPostType()==2 && p.getPostPid()==id){
+                sposts.add(p);
+                
+            }
+        if(sposts != null){
+            for(Post p : sposts){
+                x = ((p.getPostScore()*0.65) + (com.getUsers().getUser(p.getPostOwner()).getUserRep()*0.25) + (p.getPostNumCom()*0.1));
+                if(x>media){ 
+                    media = x;
+                    ret = p.getPostId();
+                }
+            }
+        }
+        return ret;        
     }
-*/
 
     //Query 11
     /*
